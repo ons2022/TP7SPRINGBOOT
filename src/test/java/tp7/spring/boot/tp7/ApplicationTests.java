7331c6d74e0fbcec6cab42ea6d4f7bc3a4e731fd
ons2022 <onsismailhajri@gmail.com>
2023-12-06 11:43:43 +0100

    first

---
+++ src/test/java/tp7/spring/boot/tp7/ApplicationTests.java
@@ -0,0 +1,13 @@
+package tp7.spring.boot.tp7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
